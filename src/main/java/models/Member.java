ed32e6a63ba7f04659b3765a3df69d1d2530b0a7
JakobLarsen <jsl28@hotmail.dk>
2017-05-19 13:11:11 +0200

    Added icons
    Pls don't break :(

--- src/main/java/models/Member.java
+++ src/main/java/models/Member.java
@@ -5,6 +5,7 @@ import org.javalite.activejdbc.annotations.Table;
 import java.util.List;
 
 @Table("members")
-public class Member extends Model {
+public class Member extends Model
+{
     public Member() {}
 }
