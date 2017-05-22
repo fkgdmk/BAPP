7bc5f3d9d819a2d55f00657554a7b250d4f41872
JakobLarsen <jsl28@hotmail.dk>
2017-05-22 09:56:45 +0200

    Fine tunined MessageService
    Added comments
    Changed layout generation for new message entries

--- src/main/java/services/MessageService.java
+++ src/main/java/services/MessageService.java
@@ -1,6 +1,7 @@
 package services;
 
 import com.jfoenix.controls.JFXTextArea;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -35,11 +36,6 @@ public class MessageService
         container = _container;
     }
 
-    public void changeDateLabel(Timestamp date)
-    {
-        dateLabel.setText(date.toString());
-    }
-
     public void saveMessage()
     {
         MessageTableSeeder messageTableSeeder = new MessageTableSeeder();
@@ -51,59 +47,76 @@ public class MessageService
         //Get messages in DB and store them in a list
         List<Message> list = Message.findAll();
 
-        //Count of message entries in VBox
+        //Count message entries in VBox
         int amountOfChildren = container.getChildren().size();
         System.out.println("AOC: " + amountOfChildren);
-        //Check to see how many iterations of the database messages is needed
+        //Count messages in database
         int amountOfMessages = list.size();
         System.out.println("AOM: " + amountOfMessages);
-        //Get difference
+        //Calculate how many many message entries need to be generated
         int difference = amountOfMessages - amountOfChildren;
 
         //If we have more messages in the database, than there are
         //entries (HBoxes) in the container (VBox), we have to create new ones.
         if (difference > 0)
         {
-            System.out.println("There is a difference of: " + difference + ". Creating new entries...");
+            System.out.println("There is a difference of " + difference + ". Creating new entries...");
+            //Create message entries as needed
             for (int i = 0; i < difference; i++)
             {
-                HBox newHBox = new HBox();
+                HBox newHBox = new HBox(40);
+                newHBox.setPadding(new Insets(0, 0, 15, 0));
+                newHBox.setPrefHeight(20);
+
                 Label sub = new Label();
+                sub.setPrefWidth(120);
                 Label rec = new Label();
+                rec.setPrefWidth(120);
                 Label sen = new Label();
+                sen.setPrefWidth(120);
                 Label dat = new Label();
+                dat.setPrefWidth(120);
                 newHBox.getChildren().addAll(sub, rec, sen, dat);
 
                 container.getChildren().add(amountOfMessages-(difference), newHBox);
             }
         }
 
-        System.out.println("AOC: " + amountOfChildren);
+        //Recalculate amount of message entries (because we just generated new ones)
         amountOfChildren = container.getChildren().size();
+
         //Loop through all VBox-entries
         for (int i = 0; i < amountOfChildren; i++)
         {
             //Grab an entry (child @ i)
             HBox messageInfo = (HBox) container.getChildren().get(i);
 
+            //Set entry labels to show the corresponding value from the database
             Label subject_Label = (Label) messageInfo.getChildren().get(0);
             subject_Label.setText(list.get(i).get("message").toString());
 
+            //FIXME: Set database values (currently hardcoded)
+            Label recipient_Label = (Label) messageInfo.getChildren().get(1);
+            recipient_Label.setText("Modtager");
+
+            //FIXME: Set database values (currently hardcoded)
+            Label sender_Label = (Label) messageInfo.getChildren().get(2);
+            sender_Label.setText("Afsender");
+
             Label date_Label = (Label) messageInfo.getChildren().get(3);
             date_Label.setText(list.get(i).get("created_at").toString());
-
-            //Grab a message (index x in the list of messages)
-            //grab the data of the message
-            //set subject of child 0 (subject field) to be that of the message in the database
-
-            //subject.setText(messageInstance.);
-
-            //set recipient of child 1 to be that of the message in the database
-
-            //set sender of child 2 to be that of the message in the database
-
-            //set date of child 3 (date field) to be that of the message in the database
         }
+    }
+
+    public void deleteSentMessage(int index)
+    {
+        //delete from database
+        List<Message> messages = Message.findAll();
+        Message messageToDelete = messages.get(index);
+        messageToDelete.delete();
+
+        //delete entry
+        container.getChildren().remove(index);
     }
 
     public void sendMessage()
